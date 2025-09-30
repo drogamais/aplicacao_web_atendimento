2da8de6a8d1b3048f764d11ae267e2bd3b70928a
Gian <analista01.altaneiro@drogamais.com.br>
2025-09-30 09:20:37 -0300

    finish convenio massa

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def get_date_rules():
     today = date.today()
     min_date_obj = None
     if DATE_FILTER_ENABLED:
-        min_date_obj = today - timedelta(days=24)
+        min_date_obj = today - timedelta(days=30)
     return min_date_obj, today
 
 def is_valid_date(date_str):
@@ -231,7 +231,7 @@ def editar_dados():
     # Combina a lista do banco com a lista do 'constants' e remove duplicatas
     lista_completa_responsaveis = sorted(list(set(responsaveis_no_banco + RESPONSAVEIS_OPCOES)))
 
-    data_corte = date.today() - timedelta(days=24)
+    data_corte = date.today() - timedelta(days=30)
 
     return render_template(
         'editar.html', 
